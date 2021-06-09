90393df0acd7f0c7beada2ceb8210950d6083c4a
Amith Saligrama <saligrama.amith@gmail.com>
2021-06-09 14:50:13 -0400

    Fields can now be left blank

--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 4. Run `vagrant ssh`
 5. Once you have a terminal in the vagrant virtual machine, run `cd bathroom-map && python3 manage.py runserver 0.0.0.0:8000`
 6. Map should load at `http://localhost:8080`.
+7. Map markers can be added at `http://localhost:8080/admin`: click on the `Add` button next to `Bathrooms`, and enter the info. You can leave latitude/longitude blank or enter zeros, it'll autocompute it from your address and zip.
--- bathroom_map/models.py
+++ bathroom_map/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Bathroom(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, blank=True)
     address = models.CharField(max_length=511)
     zip = models.CharField(max_length=5)
-    latitude = models.DecimalField(max_digits=9, decimal_places=6)
-    longitude = models.DecimalField(max_digits=9, decimal_places=6)
-    hours = models.CharField(max_length=255)
-    remarks = models.TextField()
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True)
+    hours = models.TextField(blank=True)
+    remarks = models.TextField(blank=True)
